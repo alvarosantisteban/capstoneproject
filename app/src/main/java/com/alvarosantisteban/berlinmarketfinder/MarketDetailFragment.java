2b15ad4a6897d6d167e3bf1209881031a02fd27c
Alvaro Santisteban <santisteban@karlmax-berlin.com>
2018-03-01 12:30:44 +0100

    Configure Android Studio for a master detail project.

---
+++ app/src/main/java/com/alvarosantisteban/berlinmarketfinder/MarketDetailFragment.java
@@ -0,0 +1,71 @@
+package com.alvarosantisteban.berlinmarketfinder;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.alvarosantisteban.berlinmarketfinder.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Market detail screen.
+ * This fragment is either contained in a {@link MarketListActivity}
+ * in two-pane mode (on tablets) or a {@link MarketDetailActivity}
+ * on handsets.
+ */
+public class MarketDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MarketDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null && getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            assert activity != null;
+            CollapsingToolbarLayout appBarLayout = activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.market_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.market_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
