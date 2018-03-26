ff43dd0a6094d07cfc7ada39f614db4a7dced147
Alvaro Santisteban <santisteban@karlmax-berlin.com>
2018-03-26 14:11:28 +0200

    Go to the detail site when clicking on a marker's title.

--- app/src/main/java/com/alvarosantisteban/berlinmarketfinder/MapActivity.java
+++ app/src/main/java/com/alvarosantisteban/berlinmarketfinder/MapActivity.java
@@ -1,6 +1,9 @@
 package com.alvarosantisteban.berlinmarketfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
 import com.alvarosantisteban.berlinmarketfinder.model.Market;
@@ -9,6 +12,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
@@ -37,16 +41,35 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        List<Market> markets = getIntent().getParcelableArrayListExtra(ARG_MARKETS);
+        final List<Market> markets = getIntent().getParcelableArrayListExtra(ARG_MARKETS);
+        googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(MapActivity.this, MarketDetailActivity.class);
+                intent.putExtra(MarketDetailFragment.ARG_ITEM, getMarketFromId(markets, marker.getTag() != null ? (String) marker.getTag() : ""));
+                startActivity(intent);
+            }
+        });
 
         if (markets != null) {
             for (Market market : markets) {
                 LatLng marketPos = new LatLng(market.getLatitude(), market.getLongitude());
-                googleMap.addMarker(new MarkerOptions().position(marketPos).title(market.getName()));
+                Marker marker = googleMap.addMarker(new MarkerOptions().position(marketPos).title(market.getName()));
+                marker.setTag(market.getId());
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(marketPos));
                 googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
             }
         }
+    }
+
+    @Nullable
+    private Market getMarketFromId(@NonNull List<Market> marketsList, @NonNull String marketId) {
+        for (Market market : marketsList) {
+            if(marketId.equals(market.getId())) {
+                return market;
+            }
+        }
+        return null;
     }
 }
 
